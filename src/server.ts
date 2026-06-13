c99aca1985d733bc5a4bfe1134b6ec7845f6b2b6
Fahim Farhad <fahim.farhad132@gmail.com>
2026-06-13 11:50:34 +0600

    project setup

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
+const port = 3000;
+
+
+app.use(express.json())
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.post('/', (req : Request, res : Response) =>{
+    console.log(req.body);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
